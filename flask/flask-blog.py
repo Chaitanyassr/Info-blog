06391f6bed93d19c3527a5df03d1c4b36c8d014a
Chaitanya <chaitanyas0101@gmail.com>
2020-03-28 03:39:24 +0530

    added home page and about page

--- flask/flask-blog.py
+++ flask/flask-blog.py
@@ -4,9 +4,19 @@ from flask import Flask
 app = Flask(__name__)
 
 '''route is a place were web app will open'''
+'''
 @app.route('/')
 def hello_world():
+    return '<h1>Home page</h1>' '''
+
+@app.route('/')
+@app.route('/home')
+def home():
     return '<h1>Home page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
 
 '''makingdebug = true so to run the app in debut mode so tha tpage can update automatically'''
 if __name__ == '__main__':
