48d14b6d6b483a2f0af3ec6268038dc2833fab17
Chaitanya <chaitanyas0101@gmail.com>
2020-03-28 03:35:16 +0530

    added debug mode

--- flask/flask-blog.py
+++ flask/flask-blog.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 '''route is a place were web app will open'''
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Home page</h1>'
 
+'''makingdebug = true so to run the app in debut mode so tha tpage can update automatically'''
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=true)
