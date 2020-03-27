900673e5b554173e4bd439ad29ad173d1d7ff1e0
Chaitanya <chaitanyas0101@gmail.com>
2020-03-28 03:32:12 +0530

    added h1 tag

--- flask/flask-blog.py
+++ flask/flask-blog.py
@@ -3,10 +3,10 @@ from flask import Flask
 '''here we create an app variable using instance Flask and pass variable --name--'''
 app = Flask(__name__)
 
-
+'''route is a place were web app will open'''
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1>Hello, World!</h1>'
 
 if __name__ == '__main__':
     app.run()
